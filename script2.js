643ce424fab1cc4d4f21a784e310ccea17d99844
Josh Klein <skiz303@gmail.com>
2022-09-22 19:14:33 -0400

    updated with data atts and search feature

--- script2.js
+++ script2.js
@@ -1,52 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.querySelector('form').addEventListener('submit', (event) => {
-        event.preventDefault();
-        let url = 'https://api.spacexdata.com/v4/launches';
-        fetch(url).then(res => res.json()).then(data => showData(data));
-    });
+    const cardContainer = document.querySelector('[data-launches]')
+    const cardTemplate = document.querySelector('[data-launch-template]')
+    let startDate = document.querySelector('#start-date').value;
+    let endDate = document.querySelector('#end-date').value;
+    let launches = []
 
     document.querySelector('.scroll-to-top').addEventListener('click', () => {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    });
+        document.body.scrollTop = 0
+        document.documentElement.scrollTop = 0
+    })
 
-    function showData(items) {
-        document.querySelector('.launches').innerHTML = '';
-        let startDate = document.querySelector('#start-date').value;
-        let endDate = document.querySelector('#end-date').value;
-        let webCast;
-        let launchNotes;
+    document.querySelector('[data-search]').addEventListener('input', e => {
+        const value = e.target.value.toLowerCase()
+        launches.forEach(launch => {
+            const isVisible = launch.name.toLowerCase().includes(value)
+            launch.element.classList.toggle('hide', !isVisible)
+        })
+    })
 
-        items.forEach((item) => {
-            webCast = '';
-            launchNotes = '';
+    document.querySelector('form').addEventListener('submit', (event) => {
+        event.preventDefault()
+        fetch('https://api.spacexdata.com/v4/launches').then(res => res.json()).then(data => {
+            launches = data.map(launch => {
+                const card = cardTemplate.content.cloneNode(true).children[0]
+                if (launch.date_local > startDate && launch.date_local < endDate) {
 
-            if (item.date_local > startDate && item.date_local < endDate) {
+                    card.querySelector('[data-launch-number]').textContent += launch.flight_number
+                    card.querySelector('[data-launch-name]').textContent += launch.name
+                    card.querySelector('[data-rocket-id]').textContent += launch.rocket
+                    card.querySelector('[data-launch-date]').textContent += launch.date_local
+                    card.querySelector('[data-launch-capsules').textContent += launch.capsules.length
+                    card.querySelector('[data-launch-cores').textContent += launch.cores.length
 
-                if (item.links.webcast !== null) {
-                    webCast = `<a href="${item.links.webcast}" target="_blank" class="launch-webcast">Webcast</a>`;
+                    if (launch.links.webcast !== null) {
+                        card.querySelector('[data-launch-webcast').href += launch.links.webcast
+                    }
+
+                    if (launch.details !== null) {
+                        card.querySelector('[data-launch-notes-header]').classList.toggle('hide')
+                        card.querySelector('[data-launch-notes').textContent += launch.details
+                    }
+
+                    cardContainer.append(card)
                 }
+                return { name: launch.name, element: card }
+            })
 
-                if (item.details !== null) {
-                    launchNotes = `<h3 class="launch-notes-header">Notes</h3><p class="launch-notes">${item.details}</p>`;
-                }
+        })
 
-                document.querySelector('.launches').innerHTML +=
-                    `<div class="launch">
-                    <p class="launch-number">Flight ${item.flight_number}</p>
-                    <h1 class="launch-name">${item.name}</h1>
-                    <p>Rocket ID: ${item.rocket}</p>
-                    <p>Date: ${item.date_local}</p>
-                    <p>Capsules: ${item.capsules.length}</p>
-                    <p>Cores: ${item.cores.length}</p>
-                    ${webCast}
-                    ${launchNotes}
-                    </div>`;
-            };
+    })
 
-        });
-
-    };
-
-});
+})
